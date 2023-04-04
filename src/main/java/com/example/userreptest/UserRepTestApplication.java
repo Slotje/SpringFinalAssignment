39a010b9c5f0e40694cd0ee02aa52c6e0e440cfe
Slotje <d.slotboom01@gmail.com>
2023-04-04 13:11:51 +0200

    Working version of adding user to mysql database

---
+++ src/main/java/com/example/userreptest/UserRepTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.userreptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRepTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRepTestApplication.class, args);
+    }
+
+}
